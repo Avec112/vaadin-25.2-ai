a1b5f13c3458f20c93a79e8bc376f46a6b42131d
Avec112 <avec112@gmail.com>
2026-09-16 18:04:21 +0200

    - Changed the extend - Added "hamburger" and title - The assistants name is now model name

--- src/main/java/io/github/avec112/ai/AiChatView.java
+++ src/main/java/io/github/avec112/ai/AiChatView.java
@@ -10,13 +10,16 @@ import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import io.github.avec112.base.ui.ViewTitle;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.springframework.ai.chat.model.ChatModel;
 
 @PageTitle("Chat Bot")
 @Route("")
 @RouteAlias("chat-bot")
 @Menu(order = 0, icon = "icons/robot-line-icon.svg", title = "Chat Bot")
-public class AiChatView extends Composite<VerticalLayout> {
+public class AiChatView extends VerticalLayout {
     public AiChatView(ChatModel chatModel) {
         // Create UI components
         var messageList = new MessageList();
@@ -28,14 +31,19 @@ public class AiChatView extends Composite<VerticalLayout> {
         var provider = new SpringAILLMProvider(chatModel);
 
         // Wire everything together
+        String model = chatModel.getOptions().getModel();
+        model = model != null? model: "Mistral";
+        String modelName = StringUtils.capitalize(model);
         AIOrchestrator.builder(provider,
                         "You are a helpful assistant.")
+                .withAssistantName(modelName)
                 .withMessageList(messageList)
                 .withInput(messageInput)
                 .build();
 
         // Add UI components to the layout
-        getContent().addAndExpand(messageList);
-        getContent().add(messageInput);
+        add(new ViewTitle("Chat Bot"));
+        addAndExpand(messageList);
+        add(messageInput);
     }
 }
