ab30656d6c001187f8a1f76a19c28efadf649ffc
start.vaadin.com <no-reply@vaadin.com>
2026-09-16 06:52:07 +0000

    Generated project

---
+++ src/main/java/io/github/avec112/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.avec112.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/io/github/avec112/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package io.github.avec112.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
